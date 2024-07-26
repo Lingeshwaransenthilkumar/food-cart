90ccec43460caabe95777f700d7cc9b68b8cf25e
Lingeshwaransenthilkumar <lingeshwaransenthilkumar0279@gmail.com>
2024-07-27 00:59:25 +0530

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Header from "./header";
-import { BrowserRouter, Route, Routes  } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./home";
 import Cart from "./cart";
 import "./App.css"
@@ -13,9 +13,11 @@ function App(){
             <Header cart = {cart} setCart = {setCart} />
                <div className="container">
                    <Routes>
-                       <Route path="/" element={<Home cart={cart} setCart={setCart}/>}/>
-                       <Route path="/Cart" element={<Cart cart={cart} setCart={setCart}/>}/>
+                    <Route path="/" element={<Home cart={cart} setCart={setCart}/>}></Route>
+                    <Route path="/food-cart" element={<Home cart={cart} setCart={setCart}/>}/>
+                    <Route path="/Cart" element={<Cart cart={cart} setCart={setCart}/>}/>
                    </Routes>
+
                </div>
          </BrowserRouter>
         </>
